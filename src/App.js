c33e37d3deccdea717c7c59ca8e0ab6eb725d83b
Miles <mhale@protonmail.com>
2023-09-16 18:28:29 +0100

    Added react router with stubbed pages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,44 @@
-import Grid from './components/Grid.js'
+import React from "react";
+import Grid from './components/Grid';
+import { BrowserRouter,  Route, Switch } from "react-router-dom";
+import Transactions from './components/Transactions';
+
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <Grid />
-      </header>
+    <div className="wrapper">
+      <h1>Marine Mammals</h1>
+      <nav>
+        <ul>
+          <li><a href="/home">Home</a></li>
+          <li><a href="/transaction">Transactions</a></li>
+          <li><a href="/about">About</a></li>
+        </ul>
+      </nav>
+      <BrowserRouter>
+      <Switch>
+          <Route path="/home">
+            <Grid />
+          </Route>
+          <Route path="/transaction">
+            <Transactions />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+
+// create about component. include h1 tag with text.
+
+const About = () => (
+  <div>
+    <h1>About</h1>
+  </div>
+);
