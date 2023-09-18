c38b316c6703e9fb43ac35f15455752163f8abfa
Miles <mhale@protonmail.com>
2023-09-18 08:54:44 +0100

    Updated edit page to use bootstrap controls

--- src/components/transaction/TransactionEdit.js
+++ src/components/transaction/TransactionEdit.js
@@ -1,7 +1,7 @@
 import {useEffect} from "react";
 import {useParams} from "react-router-dom/cjs/react-router-dom.min";
 import {useState} from "react";
-import {Button} from "react-bootstrap";
+import {Button, Form, Row, Col} from "react-bootstrap";
 const serverUrl = process.env.REACT_APP_MONEYMAN_SERVER_URL;
 
 function TransactionEdit()
@@ -35,20 +35,45 @@ function TransactionEdit()
 
     return (
         <div>
-            <h1>Transaction Edit {id}</h1>
+            <h1>{transaction.name}</h1>
             
-             <form onSubmit={handleSubmit}>
-                 <label htmlFor="name">Name</label>
-                 <input id="name" name="name" type="text" defaultValue={transaction.name} />
-            
-                 <label htmlFor="amount">Amount</label>
-                 <input id="amount" name="amount" type="text" defaultValue={transaction.amount} />
-            
-                 <label htmlFor="date">Date</label>
-                 <input id="date" name="date" type="text" defaultValue={transaction.date} />
+
+
+             <Form onSubmit={handleSubmit}>
+                
+
+                <Row>
+                    <Form.Group as={Col} md="4" controlId="validationCustom02">
+                        <Form.Label>Amount</Form.Label>
+                        <Form.Control
+                            required
+                            id="amount"
+                            name="amount"
+                            type="text"
+                            placeholder="Amount"
+                            defaultValue={transaction.amount}
+                        />
+                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    </Form.Group>
+                </Row>
+
+                <Row>
+                    <Form.Group as={Col} md="4" controlId="validationCustom03">
+                        <Form.Label>Start Date</Form.Label>
+                        <Form.Control
+                            required
+                            id="date"
+                            name="date"
+                            type="date"
+                            placeholder="Start Date"
+                            defaultValue={transaction.startDate}
+                        />
+                        <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+                    </Form.Group>
+                </Row>
             
                  <button>Submit</button>
-             </form>
+             </Form>
             
 
         </div>
