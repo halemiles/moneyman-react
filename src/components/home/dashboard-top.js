5eaed12d085579d8200e143beaa02581b010ac89
Miles Hale <hale.miles@gmail.com>
2024-08-16 08:56:50 +0100

    Added some more functionality around pulling transactions around today (last and next set of transactions)

--- src/components/home/dashboard-top.js
+++ src/components/home/dashboard-top.js
@@ -1,15 +1,34 @@
 import Col from 'react-bootstrap/Col';
 import Row from 'react-bootstrap/Row';
-import TotalDueMonth from './cards/total-due-month'
-import DayTransactions from './cards/day-transactions'
+import DashboardCard from './cards/dashboard-card'
+import DashboardSummaryCard from './cards/dashboard-card'
+import {useEffect, useState} from 'react';
 
 function DashboardTop() {
+  const [actual, setActual] = useState(0);
+    const [due, setDue] = useState(0);
+  const [dashboardData, setDashboardData] = useState({tomorrow:"",today:"",yesterday:""});
+
+  useEffect(() => {
+    fetch(`${process.env.REACT_APP_MONEYMAN_SERVER_URL}/dashboard/threedays`)
+    .then((res) => res.json())
+    .then((data) => {
+      console.log(data);
+        setDashboardData({tomorrow:data.nextDates,today:data.today,yesterday:data.previousDates});
+        console.log(dashboardData.tomorrow);
+    });
+  }, []);
+
+  const handleActualChange = (e) => {
+    setActual(e.target.value);
+  };
+
   return (
     <Row xs={1} md={4} className="g-4">
-      <Col key={"totalduemonthcard"}><TotalDueMonth /></Col>
-      <Col key={"yesterdaycard"}><DayTransactions title={"Yesterday"} body={"This is a body"} /></Col>
-      <Col key={"todaycard"}><DayTransactions title={"Today"} body={"This is a body"} /></Col>
-      <Col key={"tomorrowcard"}><DayTransactions title={"Tomorrow"} body={"This is a body"} /></Col>
+      <Col key={"totalduemonthcard"}><DashboardCard subtitle={"Remaining"} title={"£100"} /></Col>
+      <Col key={"yesterdaycard"}><DashboardSummaryCard items={dashboardData.yesterday} /></Col>
+      <Col key={"todaycard"}><DashboardCard subtitle={"Today"} title={"This is a body"} /></Col>
+      <Col key={"tomorrowcard"}><DashboardCard subtitle={"Yesterday"} title={"This is a body"} /></Col>
 
     </Row>
   );
